3b13bc3c8478f0d090f6b5178ad3a0ff0fdcecc3
crk <kcl9769@naver.com>
2024-09-09 22:17:58 +0900

    회원가입, 로그인, 프로필 조회 View 작성 1. accounts/validators.py 생성하고 회원가입 및 회원 정보 수정 validation 함수 작성 2. accounts/serializer.py 생성하고 class UserSerializer 생성 3. accounts/urls.py에 회원가입, 로그인, 프로필 조회 경로 설정

--- accounts/validators.py
+++ accounts/validators.py
@@ -1,4 +1,7 @@
 from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
+from .models import User
+
 
 def validate_signup(signup_data):
     username = signup_data.get('username')
@@ -8,14 +11,36 @@ def validate_signup(signup_data):
     first_name = signup_data.get('first_name')
     last_name = signup_data.get('last_name')
     email = signup_data.get('email')
-    
+
+    err_msg_list = []
+
+    required_fields = ['username', 'password', 'nickname', 'birth', 'first_name', 'last_name', 'email']
+    for field in required_fields:
+        if not signup_data.get(field):
+            err_msg_list.append(f"{field.replace('_', ' ').capitalize()} 입력이 필요합니다")
+
     # validation
     if len(nickname) > 20:
-        return False, "닉네임은 20자 이내로 입력하세요."
+        err_msg_list.append("닉네임은 20자 이내로 입력하세요.")
 
+    if username and User.objects.filter(username=username).exists():
+        err_msg_list.append("이미 존재하는 유저네임입니다.")
+
+    # 이메일 validation
     try:
-        validate_email(email)
-    except:
-        return False, "올바른 이메일 형식이 아닙니다."
+        if email:
+            validate_email(email)
+    except ValidationError:
+        err_msg_list.append("이메일 형식이 올바르지 않습니다.")
 
-    return True, None
+    return not bool(err_msg_list), err_msg_list
+
+
+def validate_update_user(user_data):
+    nickname = user_data.get('nickname')
+
+    # validation
+    if len(nickname) < 3:
+        return False, "닉네임임은 3자 이상으로 입력하세요."
+    if len(nickname) > 20:
+        return False, "닉네임은 20자 이내로 입력하세요."
--- accounts/views.py
+++ accounts/views.py
@@ -6,55 +6,77 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .models import User
 from django.core.validators import validate_email
-from .validators import validate_signup
+from .validators import validate_signup, validate_update_user
+from .serializers import UserSerializer
+from django.contrib.auth import authenticate, login, logout
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 class SignUpView(APIView):
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        nickname = request.data.get('nickname')
-        birth = request.data.get('birth')
-        first_name = request.data.get('first_name')
-        last_name = request.data.get('last_name')
-        email = request.data.get('email')
-
         is_valid, err_msg = validate_signup(request.data)
         if not is_valid:
             return Response({"error": err_msg}, status=status.HTTP_400_BAD_REQUEST)
 
-        user = User.objects.create_user(
-            username=username,
-            password=password,
-            nickname=nickname,
-            birth=birth,
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-        )
+        user = User.objects.create_user(**request.data)
+        serializer = UserSerializer(user)
+        res_data = serializer.data
+        refresh = RefreshToken.for_user(user)
+        res_data['tokens'] = {
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }
+        return Response(res_data)
 
-        print(user)
-
-        return Response({})
-    
 
 class SignInView(APIView):
-    pass
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not User.objects.filter(username=username).exists():
+            return Response(
+                {"error": "존재하지 않는 유저네임입니다." }, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response(
+                {"error": "유효하지 않은 유저네임이나 비밀번호입니다." }, status=status.HTTP_400_BAD_REQUEST
+            )
+    
+        serializer = UserSerializer(user)
+        res_data = serializer.data 
+
+        # token
+        refresh = RefreshToken.for_user(user)
+        res_data['tokens'] = {
+            'access': str(refresh.access_token),
+            'refresh': str(refresh),
+        }
+        return Response(res_data)
+
 
 class SignOutView(APIView):
     pass
 
+
 class UserProfileView(APIView):
-    pass
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
-    # def get(self, request, username):
-    #     user = User.objects.get(username=username)
-    #     serializer = UserSerialzier(user)
-    #     return Response(serializer.data)
-    
-    # def put(self, request, username):
-    #     user = User.objects.get(username=username)
-    #     nickname = request.data.get('nickname')
-    #     print(nickname)
+    def get(self, request, username):
+        user = User.objects.get(username=username)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
-    #     return Response()
+    def put(self, request, username):
+        user = User.objects.get(username=username)
+        is_valid, err_msg = validate_update_user(request.data)
+
+        nickname = request.data.get('nickname')
+        user.nickname = nickname
+        user.save()
+        
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
